1920e57829d0c6da2dc5d51f9366cb11f7db888e
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2016-03-03 23:29:55 +0200

    angular2 deps

---
+++ app/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+let omglol = "hi"
+console.log(omglol)
--- main.mk
+++ main.mk
@@ -13,6 +13,10 @@ src := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 DATA := $(src)/test/data/shevchenko
 
 mkdir = @mkdir -p $(dir $@)
+define copy =
+$(mkdir)
+cp $< $@
+endef
 
 
 export NODE_PATH = $(realpath node_modules)
@@ -22,6 +26,11 @@ node_modules: package.json
 
 package.json: $(src)/package.json
 	cp -a $< $@
+
+# automatically restart Make if there is no node_modules folder
+include node_modules.mk
+node_modules.mk: node_modules
+	touch $@
 
 
 data.src := $(shell find $(DATA) -type f)
@@ -34,7 +43,70 @@ $(data.dest): $(out)/%: $(DATA)/%
 compile: $(data.dest)
 
 
-$(out)/index.json: $(data.dest) node_modules
+$(out)/index.json: $(data.dest)  node_modules.mk
 	$(src)/index $(data.dest) > $@
 
 compile: $(out)/index.json
+
+
+npm.js.ext := .min.js .js
+ifeq ($(NODE_ENV), development)
+npm.js.ext := .dev.js .js
+endif
+npm.js.src := angular2/bundles/angular2-all.umd \
+	angular2/bundles/angular2-polyfills \
+	rxjs/bundles/Rx.umd \
+	babel-polyfill/dist/polyfill
+
+npm-get-src = $(firstword $(foreach ext,$(npm.js.ext),\
+	$(or $(wildcard node_modules/$(1)$(ext)))))
+define npm-rule =
+$(2): $$(call npm-get-src,$(1))
+	$$(copy)
+endef
+npm.js.dest := $(foreach file, $(npm.js.src), $(out)/.npm/$(file).js)
+# generate rules on the fly
+$(foreach file, $(npm.js.src), \
+	$(eval $(call npm-rule,$(file),$(out)/.npm/$(file).js)))
+
+$(npm.js.dest): node_modules.mk
+compile: $(npm.js.dest)
+
+
+app.static.src := $(wildcard $(src)/app/*.html \
+	$(src)/app/*.css $(src)/app/*.jpg $(src)/app/*.svg)
+app.static.dest := $(patsubst $(src)/app/%, $(out)/%, $(app.static.src))
+
+$(app.static.dest): $(out)/%: $(src)/app/%
+	$(copy)
+
+compile: $(app.static.dest)
+
+
+app.js.src := $(wildcard $(src)/app/*.js)
+app.js.dest.ext := .es5
+ifeq ($(NODE_ENV), development)
+app.js.dest.ext := .js
+endif
+app.js.dest := $(patsubst $(src)/app/%.js, $(out)/%$(app.js.dest.ext), $(app.js.src))
+
+babel := node_modules/.bin/babel
+ifeq ($(NODE_ENV), development)
+BABEL_OPT := -s inline
+endif
+$(out)/%$(app.js.dest.ext): $(src)/app/%.js
+	$(mkdir)
+	$(babel) --presets es2015 $(BABEL_OPT) $< -o $@
+
+
+es6.dest := $(patsubst %.es5, %.js, $(app.js.dest))
+ifeq ($(app.js.dest.ext), .es5)
+.INTERMEDIATE: $(app.js.dest)
+endif
+UGLIFYJS_OPT := --screw-ie8 -m -c
+
+%.js: %.es5
+	node_modules/.bin/uglifyjs $(UGLIFYJS_OPT) -o $@ -- $<
+
+$(es6.dest): node_modules.mk
+compile: $(es6.dest)
